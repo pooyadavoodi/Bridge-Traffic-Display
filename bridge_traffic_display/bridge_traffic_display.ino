966f6a66e76d46a52b12ded7e1daab612eb45eaa
Pooya Davoodi <pooyadavoodi@gmail.com>
2018-07-05 22:12:01 -0700

    Add multi-strip code with lots of other enhancements

--- bridge_traffic_display/bridge_traffic_display.ino
+++ bridge_traffic_display/bridge_traffic_display.ino
@@ -23,11 +23,6 @@
 // Additional libraries - each one of these will need to be installed.
 // ----------------------------
 
-#include <WiFiManager.h>
-// For configuring the Wifi credentials without re-programing 
-// Availalbe on library manager (WiFiManager)
-// https://github.com/tzapu/WiFiManager
-
 #include <GoogleMapsApi.h>
 // For accessing Google Maps Api
 // Availalbe on library manager (GoogleMapsApi)
@@ -37,13 +32,6 @@
 // For parsing the response from google maps and for the config file
 // Available on the library manager (ArduinoJson)
 // https://github.com/bblanchon/ArduinoJson
-
-#include <DoubleResetDetector.h>
-// For entering Config mode by pressing reset twice
-// Not yet available on the library manager
-// Go to the github page and there is a download button
-// Click Download as zip, and add to Arduino IDE(Sketch->Include Library-> Add .zip library)
-// https://github.com/datacute/DoubleResetDetector
 
 #include <Adafruit_NeoPixel.h>
 // For controlling the Addressable LEDs
@@ -56,22 +44,21 @@
 // https://github.com/arduino-libraries/NTPClient
 
 
-// The name of the config file stored on SPIFFS, it will create this if it doesn't exist
-#define BRIDGE_CONFIG_FILE "bridge.config"
-
 #define GREEN_COLOUR_INDEX 0
 #define YELLOW_COLOUR_INDEX 1
 #define RED_COLOUR_INDEX 2
 
+#define GREEN_VAL 0, 255, 0
+#define YELLOW_VAL 255, 100, 0
+#define RED_VAL 255, 0, 0
+
+#define LIGHT_GREEN_VAL 50, 200, 50
+#define LIGHT_YELLOW_VAL 255, 130, 40
+#define LIGHT_RED_VAL 200, 50, 50
+
 // ----------------------------
 // Change the following to adapt for you
 // ----------------------------
-
-// Pin that your addressable LEDS are connected to (0 is D3 on a Wemos)
-#define LED_PIN 0
-
-// Total number of addressable LEDs connected
-#define NUMBER_OF_LEDS 20
 
 // Set between 0 and 255, 255 being the brigthest
 #define BRIGTHNESS 16
@@ -89,17 +76,12 @@ const char timeServer[] = "us.pool.ntp.org";
 #define BAD_TRAFFIC_THRESHOLD 300
 
 // Default Traffic-Matrix API key, you can set this if you want or put it in using the WiFiManager
-char apiKey[45] = "";
+char apiKey[45] = "ENTER API KEY HERE";
 
 //Free Google Maps Api only allows for 2500 "elements" a day
 unsigned long delayBetweenApiCalls = 1000 * 60; // 1 mins
 
 unsigned long delayBetweenLedChange = 1000 * 10; // 10 seconds
-
-
-//Where journey should start and end
-String origin = "37.8292957,-122.2943918"; // Oakland Side
-String destination = "37.7888397,-122.3879729"; // San Fran
 
 // You also may need to change the colours in the getRouteColour method.
 // The leds I used seemed to have the Red colour and Green colour swapped in comparison 
@@ -108,8 +90,6 @@ String destination = "37.7888397,-122.3879729"; // San Fran
 // ----------------------------
 // End of area you need to change
 // ----------------------------
-
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUMBER_OF_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 WiFiUDP ntpUDP;
 
@@ -123,8 +103,6 @@ NTPClient timeClient(ntpUDP, timeServer, 3600, 60000);
 // RTC Memory Address for the DoubleResetDetector to use
 #define DRD_ADDRESS 0
 
-DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
-
 WiFiClientSecure client;
 GoogleMapsApi *mapsApi;
 
@@ -132,266 +110,241 @@ unsigned long api_due_time = 0;
 
 unsigned long led_due_time = 0;
 
-uint32_t colour;
-
-int colourIndex = 0;
-
-// flag for saving data
-bool shouldSaveConfig = false;
-
-//callback notifying us of the need to save config
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
-}
-
-void configModeCallback (WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
-
-  //We might want to indicate on the LEDS somehow that we are in config mode
-  
-  drd.stop();
-}
-
-void setup() {
-
-  Serial.begin(115200);
-
-  leds.begin(); // This initializes the NeoPixel library.
-  leds.setBrightness(BRIGTHNESS);
-  unLightAllLeds();
-
-  if (!SPIFFS.begin()) {
-    Serial.println("Failed to mount file system");
-    return;
+void connectWIFI() {
+  const char* ssid     = "ENTER WIFI SSID HERE";
+  const char* password = "ENTER WIFI PASSWORD HERE";
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-
-  loadConfig();
-
-  WiFiManager wifiManager;
-  wifiManager.setAPCallback(configModeCallback);
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-  // Adding an additional config on the WIFI manager webpage for the API Key
-  WiFiManagerParameter customApiKey("apiKey", "API Key", apiKey, 50);
-  wifiManager.addParameter(&customApiKey);
-
-  if (drd.detectDoubleReset()) {
-    Serial.println("Double Reset Detected");
-    wifiManager.startConfigPortal("BayDisplayConf", "thepassword");
-  } else {
-    Serial.println("No Double Reset Detected");
-    wifiManager.autoConnect("BayDisplayConf", "thepassword");
-  }
-
-  strcpy(apiKey, customApiKey.getValue());
-
-  if (shouldSaveConfig) {
-    saveConfig();
-  }
-
-  mapsApi = new GoogleMapsApi(apiKey, client);
-
-  colour = leds.Color(255, 0, 0);
-
-  Serial.println("WiFi connected");
+  Serial.println("");
+  Serial.println("WiFi connected");  
   Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
-  timeClient.begin();
-  drd.stop();
+  Serial.println(WiFi.localIP());
 }
 
-bool loadConfig() {
-  File configFile = SPIFFS.open(BRIDGE_CONFIG_FILE, "r");
-  if (!configFile) {
-    Serial.println("Failed to open config file");
-    return false;
-  }
+class Strip {
+  public:
+    uint32_t getColour(int durationTraffic_value, int duration_value) {
 
-  size_t size = configFile.size();
-  if (size > 1024) {
-    Serial.println("Config file size is too large");
-    return false;
-  }
+      int difference = durationTraffic_value - duration_value;
+      Serial.print("durationTraffic_value: ");
+      Serial.println(durationTraffic_value);
 
-  // Allocate a buffer to store contents of the file.
-  std::unique_ptr<char[]> buf(new char[size]);
+      Serial.print("duration_value: ");
+      Serial.println(duration_value);
 
-  configFile.readBytes(buf.get(), size);
+      Serial.print("difference: ");
+      Serial.println(difference);
 
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.parseObject(buf.get());
+      Serial.print("BAD_TRAFFIC_THRESHOLD: ");
+      Serial.println(BAD_TRAFFIC_THRESHOLD);
 
-  if (!json.success()) {
-    Serial.println("Failed to parse config file");
-    return false;
-  }
+      Serial.print("MEDIUM_TRAFFIC_THRESHOLD: ");
+      Serial.println(MEDIUM_TRAFFIC_THRESHOLD);
 
-  strcpy(apiKey, json["mapsApiKey"]);
-  return true;
-}
-
-bool saveConfig() {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.createObject();
-  json["mapsApiKey"] = apiKey;
-
-  File configFile = SPIFFS.open(BRIDGE_CONFIG_FILE, "w");
-  if (!configFile) {
-    Serial.println("Failed to open config file for writing");
-    return false;
-  }
-
-  json.printTo(configFile);
-  return true;
-}
-
-uint32_t getColour(int durationTraffic_value, int duration_value) {
-
-  int difference = durationTraffic_value - duration_value;
-
-  if(difference > BAD_TRAFFIC_THRESHOLD) {
-    Serial.println("Setitng Colour to red");
-    colourIndex = RED_COLOUR_INDEX;
-    return leds.Color(0, 255, 0); // Red
-  } else if ( difference > MEDIUM_TRAFFIC_THRESHOLD ) {
-    Serial.println("Setitng Colour to yellow");
-    colourIndex = YELLOW_COLOUR_INDEX;
-    return leds.Color(60, 180, 0); // Yellow
-  }
-  Serial.println("Setitng Colour to Green");
-  colourIndex = GREEN_COLOUR_INDEX;
-  return leds.Color(255, 0, 0); //Green
-}
-
-void unLightAllLeds() {
-  for(int i=0; i< NUMBER_OF_LEDS; i++) {
-    leds.setPixelColor(i, leds.Color(0, 0, 0));
-  }
-  leds.show();
-}
-
-void setAllLeds(uint32_t col) {
-  for(int i=0; i< NUMBER_OF_LEDS; i++) {
-    leds.setPixelColor(i, col);
-  }
-}
-
-void lightLeds(uint32_t col) {
-  unLightAllLeds();
-  for (int j = 1; j <= NUMBER_OF_LEDS; j++) {
-    for (int i = 0; i < j; i++) {
-      leds.setPixelColor(i, col);
-    }
-    leds.show();
-    delay(100);
-  }
-
-  // having the double for loop and the leds.show and delay inside the outer one
-  // is to create the one after another effect, that i think is quite nice
-  // its a good way of indicating which way it is checking traffic and also
-  // when it refreshed. We may need to change this based on how the LEDS are wired.
-  // comment out leds.show() and the delay from above and uncomment the line below to remove that feature
-  //leds.show()
-}
-
-void lightLedsForwards(uint32_t newColour, uint32_t oldColour){
-  for (int j = 1; j <= NUMBER_OF_LEDS; j++) {
-    setAllLeds(oldColour);
-    for (int i = 0; i < j; i++) {
-      leds.setPixelColor(i, newColour);
-    }
-    leds.show();
-    delay(200);
-  }
-}
-
-void lightLedsBackwards(uint32_t newColour, uint32_t oldColour){
-  for (int j = NUMBER_OF_LEDS - 1; j > 0 ; j--) {
-    setAllLeds(oldColour);
-    for (int i = NUMBER_OF_LEDS - 1; i >= j; i--) {
-      leds.setPixelColor(i, newColour);
-    }
-    leds.show();
-    delay(200);
-  }
-}
-
-void twinkleLed(){
-  uint32_t lighterColour = leds.Color(0, 0, 0);
-  switch(colourIndex){
-    case GREEN_COLOUR_INDEX:
-      {
-        lighterColour = leds.Color(80, 0, 0);
+      if(difference > BAD_TRAFFIC_THRESHOLD) {
+        Serial.println("Setitng Colour to red");
+        colourIndex = RED_COLOUR_INDEX;
+        return leds.Color(RED_VAL); // Red
+      } else if ( difference > MEDIUM_TRAFFIC_THRESHOLD ) {
+        Serial.println("Setitng Colour to yellow");
+        colourIndex = YELLOW_COLOUR_INDEX;
+        return leds.Color(YELLOW_VAL); // Yellow
       }
-      break;
-    case YELLOW_COLOUR_INDEX:
-      {
-        lighterColour = leds.Color(15, 45, 0);
+      Serial.println("Setitng Colour to Green");
+      colourIndex = GREEN_COLOUR_INDEX;
+      return leds.Color(GREEN_VAL); //Green
+    }
+
+    void unLightAllLeds() {
+      for(int i=0; i< NUMBER_OF_LEDS; i++) {
+        leds.setPixelColor(i, leds.Color(0, 0, 0));
       }
-      break;
-     
-    case RED_COLOUR_INDEX:
-      {
-        lighterColour = leds.Color(0, 80, 0);
+      leds.show();
+    }
+
+    void setAllLeds(uint32_t col) {
+      for(int i=0; i< NUMBER_OF_LEDS; i++) {
+        leds.setPixelColor(i, col);
       }
-      break;
-  }
+    }
 
-  lightLedsForwards(lighterColour, colour);
-  delay(600);
-  lightLedsBackwards(colour, lighterColour);
-}
-
-bool checkGoogleMaps() {
-  Serial.println("Getting traffic for " + origin + " to " + destination);
-    String responseString = mapsApi->distanceMatrix(origin, destination, "now");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& response = jsonBuffer.parseObject(responseString);
-    if (response.success()) {
-      if (response.containsKey("rows")) {
-        JsonObject& element = response["rows"][0]["elements"][0];
-        String status = element["status"];
-        if(status == "OK") {
-
-          int durationInSeconds = element["duration"]["value"];
-          int durationInTrafficInSeconds = element["duration_in_traffic"]["value"];
-          colour = getColour(durationInTrafficInSeconds, durationInSeconds);
-          Serial.println("Duration In Traffic:  " + durationInTrafficInSeconds);
-          return true;
-
+    void lightLeds() {
+      unLightAllLeds();
+      for (int j = 1; j <= NUMBER_OF_LEDS; j++) {
+        for (int i = 0; i < j; i++) {
+          leds.setPixelColor(i, colour);
         }
-        else {
-          Serial.println("Got an error status: " + status);
+        leds.show();
+        delay(100);
+      }
+
+      // having the double for loop and the leds.show and delay inside the outer one
+      // is to create the one after another effect, that i think is quite nice
+      // its a good way of indicating which way it is checking traffic and also
+      // when it refreshed. We may need to change this based on how the LEDS are wired.
+      // comment out leds.show() and the delay from above and uncomment the line below to remove that feature
+      //leds.show()
+    }
+
+    void lightLedsForwards(uint32_t newColour, uint32_t oldColour){
+      for (int j = 1; j <= NUMBER_OF_LEDS; j++) {
+        setAllLeds(oldColour);
+        for (int i = 0; i < j; i++) {
+          leds.setPixelColor(i, newColour);
+        }
+        leds.show();
+        delay(200);
+      }
+    }
+
+    void lightLedsBackwards(uint32_t newColour, uint32_t oldColour){
+      for (int j = NUMBER_OF_LEDS - 1; j >= 0 ; j--) {
+        setAllLeds(oldColour);
+        for (int i = NUMBER_OF_LEDS - 1; i >= j; i--) {
+          leds.setPixelColor(i, newColour);
+        }
+        leds.show();
+        delay(200);
+      }
+    }
+
+    void twinkleLed(){
+      uint32_t lighterColour = leds.Color(0, 0, 0);
+      switch(colourIndex){
+        case GREEN_COLOUR_INDEX:
+          {
+            lighterColour = leds.Color(LIGHT_GREEN_VAL);
+          }
+          break;
+        case YELLOW_COLOUR_INDEX:
+          {
+            lighterColour = leds.Color(LIGHT_YELLOW_VAL);
+          }
+          break;
+         
+        case RED_COLOUR_INDEX:
+          {
+            lighterColour = leds.Color(LIGHT_RED_VAL);
+          }
+          break;
+      }
+
+      lightLedsForwards(lighterColour, colour);
+      delay(600);
+      lightLedsBackwards(colour, lighterColour);
+    }
+
+    bool checkGoogleMaps() {
+      Serial.println("Getting traffic for " + origin + " to " + destination);
+        String responseString = mapsApi->distanceMatrix(origin, destination, "now");
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& response = jsonBuffer.parseObject(responseString);
+        if (response.success()) {
+          if (response.containsKey("rows")) {
+            JsonObject& element = response["rows"][0]["elements"][0];
+            String status = element["status"];
+            if(status == "OK") {
+
+              int durationInSeconds = element["duration"]["value"];
+              int durationInTrafficInSeconds = element["duration_in_traffic"]["value"];
+              colour = getColour(durationInTrafficInSeconds, durationInSeconds);
+              Serial.println("Duration In Traffic:  " + durationInTrafficInSeconds);
+              return true;
+
+            }
+            else {
+              Serial.println("Got an error status: " + status);
+              return false;
+            }
+          } else {
+            Serial.println("Reponse did not contain rows");
+            return false;
+          }
+        } else {
+          if(responseString == ""){
+            Serial.println("No response, probably timed out");
+          } else {
+            Serial.println("Failed to parse Json. Response:");
+            Serial.println(responseString);
+          }
+
           return false;
         }
-      } else {
-        Serial.println("Reponse did not contain rows");
-        return false;
-      }
-    } else {
-      if(responseString == ""){
-        Serial.println("No response, probably timed out");
-      } else {
-        Serial.println("Failed to parse Json. Response:");
-        Serial.println(responseString);
-      }
 
-      return false;
+        return false;
     }
 
-    return false;
-}
+    Strip(int p, int n):
+      LED_PIN(p),
+      NUMBER_OF_LEDS(n),
+      leds(NUMBER_OF_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800) {}
 
+    void initialize(String o, String d) {
+      leds.begin(); // This initializes the NeoPixel library.
+      leds.setBrightness(BRIGTHNESS);
+      unLightAllLeds();
+      colour = leds.Color(255, 0, 0);
+      setLocations(o, d);
+    }
+  private:
+    uint32_t LED_PIN;
+    uint32_t NUMBER_OF_LEDS;
+    Adafruit_NeoPixel leds;
+    String origin;
+    String destination;
+    uint32_t colour;
+    int colourIndex = 0;
+    
+    void setLocations(String o, String d) {
+      origin = o;
+      destination = d;
+    }
+};
+
+Strip strip1(1, 5);
+Strip strip3(3, 5);
+Strip strip4(4, 5);
+Strip strip5(5, 5);
+
+void setup() {
+  Serial.begin(115200);
+
+  connectWIFI();
+
+  // Denmark
+  strip1.initialize("56.168580,10.116812", "56.171801,10.187780");
+
+  // Shiraz
+  strip3.initialize("29.636075,52.480785", "29.633292,52.507145");
+
+  // Tehran
+  strip4.initialize("35.730466,51.298777", "35.701124,51.396211");
+
+  // SF Bay area
+  strip5.initialize("37.349147,-121.994421", "37.367907,-121.916010");
+  
+  mapsApi = new GoogleMapsApi(apiKey, client);
+  timeClient.begin();
+}
 
 void loop() {
   unsigned long timeNow = millis();
   if (timeNow > api_due_time)  {
     Serial.println("Checking maps");
-    if (checkGoogleMaps()) {
-      lightLeds(colour);
+    if (strip1.checkGoogleMaps()) {
+      strip1.lightLeds();
+    } 
+    if (strip3.checkGoogleMaps()) {
+      strip3.lightLeds();
+    } 
+    if (strip4.checkGoogleMaps()) {
+      strip4.lightLeds();
+    } 
+    if (strip5.checkGoogleMaps()) {
+      strip5.lightLeds();
     } 
     api_due_time = timeNow + delayBetweenApiCalls;
     led_due_time = timeNow + delayBetweenLedChange;
@@ -399,7 +352,11 @@ void loop() {
   timeNow = millis();
   if (timeNow > led_due_time)  {
     Serial.println("Chaning LED");
-    twinkleLed();
+    strip1.twinkleLed();
+    strip3.twinkleLed();
+    strip4.twinkleLed();
+    strip5.twinkleLed();
     led_due_time = timeNow + delayBetweenLedChange;
   }
 }
+
